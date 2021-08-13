8647acb5fde97a4035f2c6e6fedbcc47cc2c4f4d
Morgan.Liew <morgan_liew@qq.com>
2021-08-13 14:46:16 +0800

    update the record about JS 运算符

---
+++ test.js
@@ -0,0 +1,16 @@
+let a = 0;
+a++;
+
+console.log(a);
+console.log(a);
+console.log(a);
+console.log(a);
+let b = a + 2;
+console.log(b);
+b++;
+a++;
+console.log(a)
+console.log(b)
+
+console.log('\u2706')
+
